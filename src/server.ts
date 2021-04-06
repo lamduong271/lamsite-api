38c5e1af6d91d02815331b932d12ffed5b89c6bc
lam.duong <lam.duong@sok.fi>
2021-04-06 23:33:39 +0300

    init

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,60 @@
-import dotenv from 'dotenv'
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import { createConnection } from "typeorm"
-import {Experience} from './entities/Experiences'
-import { buildSchema } from "type-graphql"
-import { ExperienceResolver } from './resolvers/ExperienceResolver'
-import { Education } from './entities/Education'
-import { EducationResolver } from './resolvers/EducationResolver'
+import dotenv from "dotenv";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { createConnection, ConnectionOptions } from "typeorm";
+import { Experience } from "./entities/Experiences";
+import { buildSchema } from "type-graphql";
+import { ExperienceResolver } from "./resolvers/ExperienceResolver";
+import { Education } from "./entities/Education";
+import { EducationResolver } from "./resolvers/EducationResolver";
 
-dotenv.config()
+
+dotenv.config();
 
 const main = async () => {
-  await createConnection({
+  let connectionOptions: ConnectionOptions;
+  connectionOptions = {
     type: "postgres",
-    host: "localhost",
-    port: 5432,
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
     synchronize: true,
     logging: true,
     entities: [Experience, Education]
-  });
+  };
+  if (process.env.NODE_ENV !== "development") {
+
+    Object.assign(connectionOptions, {
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+  }
+
+  await createConnection(connectionOptions);
+
+  console.log(connectionOptions.host);
 
   const server = new ApolloServer({
     schema: await buildSchema({
       resolvers: [ExperienceResolver, EducationResolver],
       validate: false
-    })
+    }),
+    introspection: true,
+    playground: true,
   });
 
   const app = express();
   server.applyMiddleware({ app });
 
-  app.listen({ port: 4000 }, () =>
-    console.log('Now browse to http://localhost:4000' + server.graphqlPath)
+  const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+  const server_host = process.env.YOUR_HOST || "0.0.0.0";
+
+  app.listen({port: process.env.PORT || 4000}, () =>
+    console.log("Now browse to " + process.env.PORT + 'path ' +server.graphqlPath)
   );
-}
+};
 main().catch(err => {
   console.log(err);
-})
+});
